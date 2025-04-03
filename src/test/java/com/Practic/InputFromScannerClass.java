8ec8a5e975f8eccdcd2875e9dbe34b46792c1cea
khiti prakash rout <kprout95@gmail.com>
2025-04-03 21:10:18 +0530

    User want to give any number or name or special charcters, that need to print

--- src/test/java/com/Practic/InputFromScannerClass.java
+++ src/test/java/com/Practic/InputFromScannerClass.java
@@ -1,4 +1,22 @@
 package com.Practic;
+import java.util.Scanner;
 
 public class InputFromScannerClass {
+    // User want to give any number or name or special charcters, that need to print
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter any thing");
+        try {
+        String  value = sc.nextLine();
+        System.out.println("Enter value is:- "+value);
+        }
+        catch (Exception e)
+        {
+            System.out.println("You have enter a wrong value");
+        }
+        finally {
+            sc.close(); // Always good practice to close the scanner
+        }
+    }
 }
