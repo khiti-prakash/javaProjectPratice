8c096675379c41705e3d8096594a3a71a79715ad
khiti prakash rout <kprout95@gmail.com>
2025-04-03 20:54:01 +0530

    User want to give any number or name or special charcters, that need to print

---
+++ src/main/java/com/vabro/App.java
@@ -0,0 +1,13 @@
+package com.vabro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
